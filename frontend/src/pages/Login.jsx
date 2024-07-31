c5084e56857a6c752c0b9d70a86808b9720e89e0
Joshua Ssemwanga <93733532+jssem001@users.noreply.github.com>
2024-07-31 23:49:30 +0300

    Navbar + Footer

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import loginimage from '../assets/Images/abstract.jpeg'
-import logo from '../assets/Images/logo.jpeg'
-import Footer from '../Layout/Footer';
+import loginimage from '../images/abstract-wavy.jpeg'
+import logo from '../images/MoringaLogo.png'
+import Footer from '../Layout/Footer'; import LandingNavbar from '../components/LandingNavbar';
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -17,7 +17,10 @@ export default function Login() {
   };
 
   return (
+    <>
+    <LandingNavbar />
     <div className="h-screen flex bg-blue-100">
+      
       <div className="lg:w-1/2 items-center justify-center">
         <img 
           src= {loginimage}
@@ -26,21 +29,21 @@ export default function Login() {
         />
       </div>
       <section className="w-full lg:w-1/2 flex items-center justify-center p-6">
-        <div className="w-full max-w-md bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full max-w-md bg-white rounded-lg shadow-md dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6">
             <div className="flex flex-col items-center mb-6">
-              <img src={logo}alt="Moringa School Logo" className="w-24 h-24 mb-4" />
-              <h1 className="text-2xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
+              <img src={logo} alt="Moringa School Logo" className="w-24 h-24 mb-4" />
+              <h1 className="text-2xl font-bold leading-tight tracking-tight text-gray-900 mb-2">
                 Project Management System
               </h1>
-            </div>
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
-              Sign in to your account
+              <h1 className="text-xl font-medium">
+              Sign In
             </h1>
+            </div>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Your email
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">
+                  Email
                 </label>
                 <input 
                   type="email" 
@@ -48,13 +51,13 @@ export default function Login() {
                   onChange={(e) => setEmail(e.target.value)} 
                   name="email" 
                   id="email" 
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
                   placeholder="name@example.com" 
                   required 
                 />
               </div>
               <div>
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">
                   Password
                 </label>
                 <input 
@@ -64,12 +67,13 @@ export default function Login() {
                   name="password" 
                   id="password" 
                   placeholder="••••••••" 
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                  // className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   required 
                 />
               </div>
               <div>
-                <label htmlFor="role" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label htmlFor="role" className="block mb-2 text-sm font-medium text-gray-900">
                   Role
                 </label>
                 <select 
@@ -77,7 +81,7 @@ export default function Login() {
                   onChange={(e) => setRole(e.target.value)} 
                   id="role" 
                   name="role" 
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
                   required
                 >
                   <option value="student">Student</option>
@@ -87,19 +91,19 @@ export default function Login() {
               </div>
               <button 
                 type="submit" 
-                className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="w-full text-black bg-orange-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:text-white hover:bg-orange-600 dark:bg-orange-200 dark:hover:bg-orange-600 dark:focus:ring-blue-800"
               >
                 Sign in
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <p className="text-center mt-4">
                 Don’t have an account yet? <Link to="/signup" className="font-medium text-blue-600 hover:underline dark:text-blue-500">Sign up</Link>
               </p>
             </form>
           </div>
         </div>
-        <Footer />
       </section>
-      
     </div>
+    <Footer />
+    </>
   );
 }
