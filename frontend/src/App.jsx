0bf1f5b291b2e836da3d4632e54b75ba77d4caf3
Joshua Ssemwanga <93733532+jssem001@users.noreply.github.com>
2024-07-31 17:38:49 +0300

    LP8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
     <BrowserRouter>
       {/* <AuthProvider> */}
         <Routes>
-          
           <Route path="/" element={<LandingPage />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/task" element={<Task />} />
