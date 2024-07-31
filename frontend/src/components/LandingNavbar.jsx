f0dacfcd7f87b85f64c2cc001730b950219dfbc0
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-07-31 09:45:07 +0300

    LandingPage6

--- frontend/src/components/LandingNavbar.jsx
+++ frontend/src/components/LandingNavbar.jsx
@@ -10,13 +10,11 @@ return(
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <h1 className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src={logo} className="h-16" alt="Moringa Logo" />
-            {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Moringa Management System
-            </span> */}
+            
           </h1>
           <div
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            // id="navbar-sticky"
+            
           >
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-green-900 md:dark:bg-white dark:border-gray-700">
               <li>
