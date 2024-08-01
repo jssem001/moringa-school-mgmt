e0ac1d1c11f644fb3969308420b19ce152400c48
Joshua Ssemwanga <93733532+jssem001@users.noreply.github.com>
2024-08-01 13:57:04 +0300

    Student Profile

--- frontend/src/pages/StudentProfile.jsx
+++ frontend/src/pages/StudentProfile.jsx
@@ -8,20 +8,13 @@ const StudentProfile = () => {
     return (
         <>
             
-            
-
             <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
             
             <div class="h-screen px-3 py-4 overflow-y-auto border-r-2 border-black"
                 style={{ backgroundImage: `url('https://s3-alpha-sig.figma.com/img/cafa/5e8f/b86bafc3853580461c326f24743c095f?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KaugZBvGBxCcbk75-tCVsg8~kC52sRyusJTyiCWdHyuVFwedfeRUdkUHb9Iad0EN8FT1FbQAuHiGqSmvv1RH-mjKkjcoKpgDlFDVtMfj4GFmHxyoPz7a77RiWQF-gyMctxCoMV8PG4zVW3UXTIKmAW-EPCbhEJcL4aA6fvUFjL~l4xKAMb3Qr2SIg2aS2fRQn~Y6ZCYuaKIlx35adx-bxNsm33toSpGsoMNDGE5pNbyAfXAyBeUDU6pSlUeoyyjURqTihFOXMvia~E~N-G-wGlosH8UTEm5zSrwtRYOfxRT7x1bvq2e6YHe2VrLRy4l3GmjbwxqrFKhrCp64NmdP2w__')` }}>
-                {/* <div className="h-screen bg-cover bg-center relative opacity-15 z-0"
-                style={{
-                backgroundImage: `url('https://s3-alpha-sig.figma.com/img/cafa/5e8f/b86bafc3853580461c326f24743c095f?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KaugZBvGBxCcbk75-tCVsg8~kC52sRyusJTyiCWdHyuVFwedfeRUdkUHb9Iad0EN8FT1FbQAuHiGqSmvv1RH-mjKkjcoKpgDlFDVtMfj4GFmHxyoPz7a77RiWQF-gyMctxCoMV8PG4zVW3UXTIKmAW-EPCbhEJcL4aA6fvUFjL~l4xKAMb3Qr2SIg2aS2fRQn~Y6ZCYuaKIlx35adx-bxNsm33toSpGsoMNDGE5pNbyAfXAyBeUDU6pSlUeoyyjURqTihFOXMvia~E~N-G-wGlosH8UTEm5zSrwtRYOfxRT7x1bvq2e6YHe2VrLRy4l3GmjbwxqrFKhrCp64NmdP2w__')`,
-                }}
-                ></div> */}
-                <div class="flex items-center ps-2.5 mb-5">
-                    <img src={logo} class="h-16 me-3 sm:h-7" alt="Moringa Logo" />
-                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-black">Moringa</span>
+                
+                <div class="flex items-center ml-4 mb-5">
+                    <img src={logo} class="h-20 w-20 rounded" alt="Moringa Logo" />
                 </div>
                 <ul class="space-y-2 font-semibold">
                     <li>
@@ -46,7 +39,7 @@ const StudentProfile = () => {
                     </li>
                     <li>
                         <div class="flex items-center p-2 text-black rounded-lg dark:text-black hover:shadow hover:bg-orange-100 group">
-                            <span class="flex-1 ms-3 whitespace-nowrap">Collaborating</span>
+                            <span class="flex-1 ms-3 whitespace-nowrap">Teams</span>
                         </div>
                     </li>
                     <li>
@@ -64,69 +57,55 @@ const StudentProfile = () => {
             </aside>
 
             <div class="p-4 sm:ml-64">
-            <div class="p-4 border-2 border-gray-700 border-dashed rounded-lg">
-            <h1>Student Profile</h1>
-                <div class="grid grid-cols-3 gap-4 mb-4">
-                    <div class="flex items-center justify-center h-24 rounded bg-gray-400 dark:bg-gray-400">
+            <div class="p-4 rounded-lg">
+                <h1 class="text-3xl  font-semibold text-center">Profile</h1>
+                <ul class="border-b-2 border-gray-700 mb-3">
+                    <li><span class="font-semibold">Name:</span> John Doe </li>
+                    <li><span class="font-semibold">Email:</span> John@gmail.com </li>
+                    <li><span class="font-semibold">Phone:</span> 0712345678 </li>
+                    <li><span class="font-semibold">Role:</span> Student</li>
+                </ul>
+                <div class="grid grid-cols-3 gap-4 mb-4 border">
+                    <div class="flex items-center justify-center h-24 rounded bg-gray-400 ">
                         <p class="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                        </svg>
+                        Courses
                         </p>
                     </div>
-                    <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                    <div class="flex items-center justify-center h-24 rounded bg-gray-400 ">
                         <p class="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                        </svg>
+                        Grades
                         </p>
                     </div>
-                    <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                    <div class="flex items-center justify-center h-24 rounded bg-gray-400 ">
                         <p class="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                        </svg>
+                        Schedule
                         </p>
                     </div>
                 </div>
-                <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                    <p class="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                        </svg>
-                    </p>
-                </div>
+                
                 <div class="grid grid-cols-2 gap-4 mb-4">
-                    <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                    <div class="flex items-center justify-center rounded bg-gray-400 h-28 ">
                         <p class="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                        </svg>
+                        My Account
                         </p>
                     </div>
-                    <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                    <div class="flex items-center justify-center rounded bg-gray-400 h-28 ">
                         <p class="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                        </svg>
+                        Academic Advising
                         </p>
                     </div>
-                    <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                    <div class="flex items-center justify-center rounded bg-gray-400 h-28 ">
                         <p class="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                        </svg>
+                        Settings
                         </p>
                     </div>
-                    <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                    <div class="flex items-center justify-center rounded bg-gray-400 h-28 ">
                         <p class="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                        </svg>
+                        Update Profile
                         </p>
                     </div>
                 </div>
-                <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+                <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-400 ">
                     <p class="text-2xl text-gray-400 dark:text-gray-500">
                         <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -134,28 +113,28 @@ const StudentProfile = () => {
                     </p>
                 </div>
                 <div class="grid grid-cols-2 gap-4">
-                    <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                    <div class="flex items-center justify-center rounded bg-gray-400 h-28 ">
                         <p class="text-2xl text-gray-400 dark:text-gray-500">
                         <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
                         </svg>
                         </p>
                     </div>
-                    <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                    <div class="flex items-center justify-center rounded bg-gray-400 h-28 ">
                         <p class="text-2xl text-gray-400 dark:text-gray-500">
                         <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
                         </svg>
                         </p>
                     </div>
-                    <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                    <div class="flex items-center justify-center rounded bg-gray-400 h-28 ">
                         <p class="text-2xl text-gray-400 dark:text-gray-500">
                         <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
                         </svg>
                         </p>
                     </div>
-                    <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                    <div class="flex items-center justify-center rounded bg-gray-400 h-28 ">
                         <p class="text-2xl text-gray-400 dark:text-gray-500">
                         <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
