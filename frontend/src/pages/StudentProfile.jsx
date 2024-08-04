b754f0a2956ffc306f126373eda42440432f8c07
Joshua Ssemwanga <93733532+jssem001@users.noreply.github.com>
2024-08-05 00:57:46 +0300

    Sunday merges

--- frontend/src/pages/StudentProfile.jsx
+++ frontend/src/pages/StudentProfile.jsx
@@ -7,55 +7,34 @@ const StudentProfile = () => {
   return (
     <>
    
-            <Sidebar />
-            
-            {/* <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-            
-            <div class="h-screen px-3 py-4 overflow-y-auto border-r-2 border-black"
-                style={{ backgroundImage: `url('https://s3-alpha-sig.figma.com/img/cafa/5e8f/b86bafc3853580461c326f24743c095f?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KaugZBvGBxCcbk75-tCVsg8~kC52sRyusJTyiCWdHyuVFwedfeRUdkUHb9Iad0EN8FT1FbQAuHiGqSmvv1RH-mjKkjcoKpgDlFDVtMfj4GFmHxyoPz7a77RiWQF-gyMctxCoMV8PG4zVW3UXTIKmAW-EPCbhEJcL4aA6fvUFjL~l4xKAMb3Qr2SIg2aS2fRQn~Y6ZCYuaKIlx35adx-bxNsm33toSpGsoMNDGE5pNbyAfXAyBeUDU6pSlUeoyyjURqTihFOXMvia~E~N-G-wGlosH8UTEm5zSrwtRYOfxRT7x1bvq2e6YHe2VrLRy4l3GmjbwxqrFKhrCp64NmdP2w__')` }}>
-                
-                <div class="flex items-center ml-4 mb-5">
-                    <img src={logo} class="h-20 w-20 rounded" alt="Moringa Logo" />
-                </div>
-                <ul class="space-y-2 font-semibold">
-                    <li>
-                        <div class="flex items-center p-2 text-black rounded-lg dark:text-black hover:shadow hover:bg-orange-100 group">
-                            <Link to='/'><span class="ms-3">Home</span></Link>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="flex items-center p-2 text-black rounded-lg dark:text-black hover:shadow hover:bg-orange-100 group">
-                            <span class="flex-1 ms-3 whitespace-nowrap">Projects</span>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="flex items-center p-2 text-black rounded-lg dark:text-black hover:shadow hover:bg-orange-100 group">
-                            <span class="flex-1 ms-3 whitespace-nowrap">Tasks</span>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="flex items-center p-2 text-black rounded-lg dark:text-black hover:shadow hover:bg-orange-100 group">
-                            <span class="flex-1 ms-3 whitespace-nowrap">Calendar</span>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="flex items-center p-2 text-black rounded-lg dark:text-black hover:shadow hover:bg-orange-100 group">
-                            <span class="flex-1 ms-3 whitespace-nowrap">Teams</span>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="flex items-center p-2 text-black rounded-lg dark:text-black hover:shadow hover:bg-orange-100 group">
-                            <span class="flex-1 ms-3 whitespace-nowrap">Dashboard</span>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="flex items-center p-2 text-black rounded-lg hover:text-white hover:bg-red-700 group">
-                            <span class="flex-1 ms-3 whitespace-nowrap">Sign Out</span>
-                        </div>
-                    </li>
-                </ul>
+      <Sidebar />
+      
+      <div className="p-4 sm:ml-64">
+        <div className="p-4 rounded-lg">
+          <h1 className="text-3xl font-semibold text-center">Profile</h1>
+          <ul className="border-b-2 border-gray-700 mb-3">
+            <li><span className="font-semibold">Name:</span> John Snow </li>
+            <li><span className="font-semibold">Email:</span> John@winterfell.com </li>
+            <li><span className="font-semibold">Phone:</span> 0712345678 </li>
+            <li><span className="font-semibold">Role:</span> Student</li>
+          </ul>
+          <div className="grid grid-cols-3 gap-4 mb-4 border">
+            <div className="flex items-center justify-center h-24 rounded bg-red-900 ">
+              <p className="text-2xl text-white">
+                Courses
+              </p>
             </div>
-            </aside> */}
+            <div className="flex items-center justify-center h-24 rounded bg-red-900 ">
+              <p className="text-2xl text-white">
+                Grades
+              </p>
+            </div>
+            <div className="flex items-center justify-center h-24 rounded bg-red-900 ">
+              <p className="text-2xl text-white">
+                Schedule
+              </p>
+            </div>
+          </div>
 
 
           <div className="grid grid-cols-2 gap-4 mb-4">
