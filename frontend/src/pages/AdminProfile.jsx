14a813631ee40692073939ea736f3d14ab008d31
Joshua Ssemwanga <93733532+jssem001@users.noreply.github.com>
2024-08-05 22:17:47 +0300

    admin+intruc profiles

--- frontend/src/pages/AdminProfile.jsx
+++ frontend/src/pages/AdminProfile.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import logo from '../images/MoringaLogo.png'
 import { Link } from 'react-router-dom'
 import Sidebar from "../components/Sidebar";
+import { UserContext } from "../context/UserContext";
 
 const AdminProfile = () => {
+    //Fetch current user
+    const { currentUser, loading } = useContext(UserContext);
+    console.log('Current User:', currentUser)
 
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (!currentUser) {
+        return <div>No user data available.</div>;
+    }
+    
+    const role = currentUser.is_admin ? "Admin" : currentUser.is_instructor ? "Instructor" : "Student";
     
     return (
         <>
             <Sidebar />
 
-
             <div class="p-4 sm:ml-64">
             <div class="p-4 rounded-lg">
                 <h1 class="text-3xl  font-semibold text-center">Profile</h1>
                 <ul class="border-b-2 border-gray-700 mb-3">
-                    <li><span class="font-semibold">Name:</span> Cersei Lannister </li>
-                    <li><span class="font-semibold">Email:</span> cersei@westeros.com </li>
-                    <li><span class="font-semibold">Phone:</span> 0712345678 </li>
-                    <li><span class="font-semibold">Role:</span> Administrator</li>
+                    <li><span className="font-semibold">Name:</span> {currentUser.name} </li>
+                    <li><span className="font-semibold">Email:</span> {currentUser.email} </li>
+                    <li><span className="font-semibold">Phone:</span> {currentUser.phone || "N/A"} </li>
+                    <li><span className="font-semibold">Role:</span> {role} </li>
                 </ul>
                 <div class="grid grid-cols-3 gap-4 mb-4 border">
                     <div class="flex items-center justify-center h-24 rounded bg-gray-700 ">
