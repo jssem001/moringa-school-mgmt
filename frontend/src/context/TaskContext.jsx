1f5f1e2bcd984ee41037315115c0a5033fb68c85
Joshua Ssemwanga <93733532+jssem001@users.noreply.github.com>
2024-08-07 18:25:24 +0300

    tasks 5

--- frontend/src/context/TaskContext.jsx
+++ frontend/src/context/TaskContext.jsx
@@ -14,26 +14,24 @@ export const TaskProvider = ({ children }) => {
   }, []);
 
 
-    // Fetch Tasks
-  const fetchTasks = async () => {
+ // Fetch Tasks
+
+ const fetchTasks = async () => {
     fetch(`${server_url}/tasks`)
       .then(response => response.json())
       .then(data => {
-        const tasksWithUserNames = data.map(task => 
-          fetchUserName(task.user_id).then(userName => ({ ...task, user_name: userName }))
-        );
-
-        Promise.all(tasksWithUserNames).then(updatedTasks => {
-          const done = updatedTasks.filter(task => task.status === 'done');
-          const pending = updatedTasks.filter(task => task.status !== 'done');
-          setTasks(pending);
-          setDoneTasks(done);
-        });
+        const done = data.filter(task => task.status === 'done');
+        const pending = data.filter(task => task.status !== 'done');
+        setTasks(pending);
+        setDoneTasks(done);
       })
       .catch(error => console.error('Failed to fetch tasks:', error));
   };
 
+
+
   // Add Task
+
   const addTask = (task) => {
     fetch(`${server_url}/tasks`, {
       method: 'POST',
@@ -44,18 +42,14 @@ export const TaskProvider = ({ children }) => {
     })
       .then(response => response.json())
       .then(data => {
-        fetchUserName(data.user_id)
-          .then(userName => {
-            data.user_name = userName;
-            if (data.status === 'done') {
-              setDoneTasks([...doneTasks, data]);
-            } else {
-              setTasks([...tasks, data]);
-            }
-          });
+        if (data.status === 'done') {
+          setDoneTasks([...doneTasks, data]);
+        } else {
+          setTasks([...tasks, data]);
+        }
       })
       .catch(error => console.error('Failed to add task:', error));
-  };    
+  };
 
   // Update Task Status
 
@@ -82,24 +76,10 @@ export const TaskProvider = ({ children }) => {
   };
 
   // Clear Done Tasks
+
   const clearDoneTasks = () => {
     setDoneTasks([]);
     }
-
-
- // Get Name from the user table and assign it to a task. Use the user ID(task.user_id) to fetch the name from the user table
-
-
-    const fetchUserName = (id) => {
-        return fetch(`${server_url}/users/${id}`)
-          .then(response => response.json())
-          .then(user => user.name)
-          .catch(error => {
-            console.error('Failed to fetch user name:', error);
-            return 'Unknown';
-          });
-      };
-
 
 
 
