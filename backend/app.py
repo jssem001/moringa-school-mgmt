d2f810552b3cb84be0363615c84037c40b20e233
Joshua Ssemwanga <93733532+jssem001@users.noreply.github.com>
2024-08-08 23:10:26 +0300

    app.db

--- backend/app.py
+++ backend/app.py
@@ -185,7 +185,7 @@ def update_profile():
     db.session.commit()
     return jsonify({"success": "User updated successfully"}), 200
 
-#Update User Role-untested 
+#Update User Role-untested--------- 
 @app.route('/user/<int:user_id>/role', methods=['PUT'])
 def update_user_role(user_id):
     data = request.get_json()
