004801350aac08237824e05ed3b0ec3c2bd07142
Joshua Ssemwanga <93733532+jssem001@users.noreply.github.com>
2024-08-09 07:30:59 +0300

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ import AddTemplate from "./pages/AddTemplate";
 import EditTemplate from "./pages/EditTemplate";
 import SingleTemplate from "./pages/SingleTemplate";
 import { UserProvider } from "./context/UserContext";
-import { ProjectProvider } from "./context/ProjectContext";
+// import { ProjectProvider } from "./context/ProjectContext";
 import { TaskProvider } from "./context/TaskContext";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
@@ -34,7 +34,7 @@ function App() {
     <TaskProvider>
     <Router>
       <UserProvider>
-        <ProjectProvider> 
+        {/* <ProjectProvider>  */}
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<Login />} />
@@ -56,7 +56,7 @@ function App() {
             <Route path="/edit-template/:templateId" element={<EditTemplate />} />
             <Route path="*" element={<div>Page Not Found</div>} />
           </Routes>
-        </ProjectProvider>
+        {/* </ProjectProvider> */}
        <ToastContainer />                        
       </UserProvider>
     </Router>
